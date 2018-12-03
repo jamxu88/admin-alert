d0d2fc43e948bcc05e10af1eee1f05f7ed485783
James <43918220+jamxu88@users.noreply.github.com>
2018-12-03 17:23:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const client = new Discord.Client();
 client.on("ready", () => {
   console.log("Parliament Bot");
 });
-const prefix = "&"
 client.on("message", (message) => {
 function react() {
   message.react("🔽");
-  globalreacts = globalreacts + 1
 }
 if(message.author.bot) return;
 if (message.content.startsWith("'")) {
@@ -15,5 +13,4 @@ if (message.content.startsWith("'")) {
   message.react("🔼");
 }
 });
-//Test Token
 client.login(process.env.BOT_TOKEN);
